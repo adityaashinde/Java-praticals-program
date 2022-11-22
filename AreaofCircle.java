28e54169a6985ea5e00f15e87b252b92d49f2a7a
Aditya Shinde <adityashinde4580@gmail.com>
2022-11-22 13:33:05 +0530

    Create AreaofCircle.java

---
+++ AreaofCircle.java
@@ -0,0 +1,14 @@
+// To calculate and display area of circle
+
+public class Circle {
+    public static void main(String[] args)
+    {
+        int radius;
+        double pi = 3.142, area;
+        radius = 5;
+        // calculating the area of the circle
+        area = pi * radius * radius;
+        // printing the area of the circle
+        System.out.println("Area of circle is :" + area);
+    }
+}
